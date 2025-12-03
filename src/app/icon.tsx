d724a5735e4bb873e5033b75029a3ead8fe1ceea
Fearless09 <toheebopeyemi9@gmail.com>
2025-12-03 10:20:05 +0100

    Fav Icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,53 @@
+import { Dumbbell } from "lucide-react";
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 40,
+  height: 40,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    // ImageResponse JSX element
+    <div
+      style={{
+        fontSize: 24,
+        backgroundImage: "linear-gradient(to bottom right, #dc2626, #f97316)",
+        borderRadius: "8px",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M17.596 12.768a2 2 0 1 0 2.829-2.829l-1.768-1.767a2 2 0 0 0 2.828-2.829l-2.828-2.828a2 2 0 0 0-2.829 2.828l-1.767-1.768a2 2 0 1 0-2.829 2.829z" />
+        <path d="m2.5 21.5 1.4-1.4" />
+        <path d="m20.1 3.9 1.4-1.4" />
+        <path d="M5.343 21.485a2 2 0 1 0 2.829-2.828l1.767 1.768a2 2 0 1 0 2.829-2.829l-6.364-6.364a2 2 0 1 0-2.829 2.829l1.768 1.767a2 2 0 0 0-2.828 2.829z" />
+        <path d="m9.6 14.4 4.8-4.8" />
+      </svg>
+    </div>,
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+}
